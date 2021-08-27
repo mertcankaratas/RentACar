b37571855d71bd9ba047b5c035d790dc85c23d66
Mertcan Karataş <can.mert.karatas@gmail.com>
2021-08-27 13:37:20 +0300

    Yeni Tablolar eklendi CRUD operasyonları yazıldı

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -59,7 +59,7 @@ namespace Business.Concrete
 
         public  IDataResult <List<CarDetailDto>> GetCarDetails()
         {
-            return new SuccessDataResult<List<CarDetailDto>> (_CarDal.GetCarDetails().ToList(),Messages.CarListed);
+            return new SuccessDataResult<List<CarDetailDto>> (_CarDal.GetCarDetails());
         }
 
         public IResult Update(Car car)
