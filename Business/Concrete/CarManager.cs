79923ab3104586e316028ecc255fb36d88a6c6ee
MERTCAN KARATAŞ <can.mert.karatas@gmail.com>
2021-09-04 17:59:12 +0300

    AOP Eklendi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constans;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -21,10 +22,10 @@ namespace Business.Concrete
         {
             _CarDal = carDal;
         }
-
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            ValidationTool.Validate(new CarValidator(), car);
+            
             _CarDal.Add(car);
 
             return new SuccessResult(Messages.CarAdded);
